a7bcce6204ca229d20cbfdb7dae0e41119d3bd6e
Jack Zellweger <20098240+jackzellweger@users.noreply.github.com>
2023-05-24 17:35:31 -0400

    Reversed columntransformer in prediction pipeline

--- flask_app/app.py
+++ flask_app/app.py
@@ -6,35 +6,62 @@ import requests
 app = Flask(__name__)
 
 # Load the model and preprocessor
-model = joblib.load('./model/model.joblib')
-preprocessor = joblib.load('./model/preprocessor.joblib')
+model = joblib.load("./model/model.joblib")
+encoder = joblib.load("./model/preprocessor.joblib")
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/predict', methods=['GET','POST'])
+
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
             # Get the data from the POST request
             data = request.get_json(force=True)
 
-            # Preprocess the data
-            processed_data = preprocessor.transform([data])
+            # Convert the data to a DataFrame
+            df = pd.DataFrame([data])  # Wrap the data in another list
+
+            # print(df.to_string())
+
+            # One-hot encode the categorical features
+            encoded_features = encoder.transform(
+                df[["BOROUGH CODE", "GROUPED CATEGORY"]]
+            )
+            processed_data = encoded_features
+
+            # Combine the numerical and encoded categorical features
+            processed_data = np.concatenate(
+                [
+                    df[
+                        [
+                            "GROSS SQUARE FEET",
+                            "LAND SQUARE FEET",
+                            "LATITUDE",
+                            "LONGITUDE",
+                            "SALE PRICE",  # FIXME: Is sale price necessary?
+                        ]
+                    ].values,
+                    encoded_features,
+                ],
+                axis=1,
+            )
 
             # Make prediction using model
             prediction = model.predict(processed_data)
 
-            # Depending on the model used, you may want to transform the prediction back to the original domain
-            # prediction = scaler_target.inverse_transform(prediction)
-
             # Return the prediction
-            return jsonify(prediction.tolist())
+            return jsonify({"prediction": prediction[0]})
+
         except Exception as err:
             return f"{err.__class__.__name__}: {err}"
+
     else:
         return "This is the prediction page!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
