ef9ebc6735e095747064d7c5be4876b72481e9bb
Jack Zellweger <20098240+jackzellweger@users.noreply.github.com>
2023-05-26 14:06:02 -0400

    Updates to main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # This script runs best on Debian
 
 # Connect to EC2 instance
@@ -5,27 +7,18 @@
 #chmod 400 key-pair-1.pem
 #ssh -i key-pair-1.pem admin@ec2-18-208-146-140.compute-1.amazonaws.com
 
-# Nav to /opt folder
-cd ../..//opt
-
 # Changed permission of the opt folder 
-sudo chown admin /opt
+# Run this scrpt 
+# cd ../..//opt && sudo chown admin /opt && sudo apt upgrade -y && sudo apt-get update -y && sudo apt-get install git -y && sudo git clone https://github.com/jackzellweger/real-estate-predictor.git && cd real-estate-predictor
 
-# Update everything
-sudo apt upgrade -y
 
-# Install git on the instance
-sudo apt-get update -y
-sudo apt-get install git -y
-# git --version # Test if git is installed
 
 # Install docker dependencies
-sudo apt-get install gnupg2
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install gnupg2 -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 # Adding gpg keys to docker
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o docker.gpg
-sudo mv docker.gpg /etc/apt/trusted.gpg.d/
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o docker.gpg && sudo mv docker.gpg /etc/apt/trusted.gpg.d/
 
 # Adding docker’s stable repo
 echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
@@ -33,31 +26,33 @@ echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -c
 # Update everything
 sudo apt-get update
 
-# Install Docker itself
-sudo apt-get install docker-ce
+# Set up Docker repo
+sudo apt-get update && sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo docker run hello-world
 
 # Verify that Docker is running (this doesn’t need to go into the script)
 # sudo systemctl status docker
 
 # Use pip to install docker compose
 sudo apt-get install python3-pip -y
-sudo pip install --upgrade docker-compose -y
+sudo pip install --upgrade docker-compose
 
 # Get into the Ec2 SQL instance
 #sudo docker exec -it real-estate-predictor_db_1 mysql -u user -p
 
 #Password from compose.yaml 
 #MYSQL_PASSWORD: password
-
-# Clone the repo
-sudo git clone https://github.com/jackzellweger/real-estate-predictor.git
-
-# Navigate into project directory
-cd real-estate-predictor
-
-# Insert a bit here about running this script 
-# ...
-
 
 # Build the compose file
 sudo docker-compose up --build
